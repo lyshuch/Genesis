ad125a8af2efc51f0bb68232258df4b63b9cead8
Austin Starost <astarost@ivytech.edu>
2019-08-25 21:21:25 -0400

    initial commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,6 +13,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Genesis");
         Scene scene = new Scene(root, 1920, 1080);
+        scene.getStylesheets().add("sample/Viper.css");
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
